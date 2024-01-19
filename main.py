1b1f297107538e0567c751706fd90ee640aab90d
aalvaropc <aalvaropc@gmail.com>
2024-01-19 15:33:07 -0500

    add allow-cors

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.numzzle import numzzle
+
 app = FastAPI(
     title="NumzzleBackend",
-    description="retorna la lista de movimientos para resolver un puzzle 3x3",
+    description="Retorna la lista de movimientos para resolver un puzzle 3x3",
     version="1.0.0",
 )
 
+# Configurar el middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes ajustar esto según tus necesidades
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Incluir las rutas
 app.include_router(numzzle)
